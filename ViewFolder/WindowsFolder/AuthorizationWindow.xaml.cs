d9012a8ff4b69b9a42de09589305cf3f2ab18ab2
Bloodthirsty Snowfla <99435523+MaR1XyAnA@users.noreply.github.com>
2023-02-14 21:12:25 +0300

    njh

--- ViewFolder/WindowsFolder/AuthorizationWindow.xaml.cs
+++ ViewFolder/WindowsFolder/AuthorizationWindow.xaml.cs
@@ -1,6 +1,6 @@
-﻿using MirzaevLibrary.AppDataFolder.ClassFolder;
-using MirzaevLibrary.ViewFolder.PageFolder;
+﻿using System;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace MirzaevLibrary.ViewFolder.WindowsFolder
@@ -10,8 +10,75 @@ namespace MirzaevLibrary.ViewFolder.WindowsFolder
         public AuthorizationWindow()
         {
             InitializeComponent();
-            FrameNavigationClass.BodyAutorizationFNC = AuthorizationFrame;
-            FrameNavigationClass.BodyAutorizationFNC.Navigate(new AuthorizationPage());
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void IgnoreAutborizationButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RegistrationButton_Click(object sender, RoutedEventArgs e)
+        {
+            RegistrationWindow registrationWindow = new RegistrationWindow();
+            registrationWindow.Show();
+            this.Close();
+        }
+
+        private void VisiblePasswordUserButton_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            CollapsedPasswordGrid.Visibility = Visibility.Collapsed;
+            VisiblePasswordGrid.Visibility = Visibility.Visible;
+            string PasswordString = Convert.ToString(PasswordPasswordBox.Password);
+            PasswordTextBox.Text = PasswordString;
+        }
+
+        private void VisiblePasswordUserButton_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            CollapsedPasswordGrid.Visibility = Visibility.Visible;
+            VisiblePasswordGrid.Visibility = Visibility.Collapsed;
+            string PasswordString = Convert.ToString(PasswordTextBox.Text);
+            PasswordPasswordBox.Password = PasswordString;
+        }
+
+        private void LoginTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (LoginTextBox.Text.Length > 0)
+            {
+                HintLoginTextBlock.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                HintLoginTextBlock.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void PasswordPasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (PasswordPasswordBox.Password.Length > 0)
+            {
+                HintPasswordVisibilityTextBlock.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                HintPasswordVisibilityTextBlock.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void PasswordTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (PasswordTextBox.Text.Length > 0)
+            {
+                HintPasswordCollapsedTextBlock.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                HintPasswordCollapsedTextBlock.Visibility = Visibility.Visible;
+            }
         }
 
         #region Управление окном
@@ -35,3 +102,4 @@ namespace MirzaevLibrary.ViewFolder.WindowsFolder
         #endregion
     }
 }
+
--- ViewFolder/WindowsFolder/RegistrationWindow.xaml.cs
+++ ViewFolder/WindowsFolder/RegistrationWindow.xaml.cs
@@ -1,6 +1,5 @@
-﻿using MirzaevLibrary.AppDataFolder.ClassFolder;
-using MirzaevLibrary.ViewFolder.PageFolder;
-using System.Windows;
+﻿using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace MirzaevLibrary.ViewFolder.WindowsFolder
@@ -10,8 +9,102 @@ namespace MirzaevLibrary.ViewFolder.WindowsFolder
         public RegistrationWindow()
         {
             InitializeComponent();
-            FrameNavigationClass.BodyRegistrationFNC = RegistrationFrame;
-            FrameNavigationClass.BodyRegistrationFNC.Navigate(new RegistrationPage());
+        }
+
+        private void SurnameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (SurnameTextBox.Text.Length > 0)
+            {
+                HintSurnameTextBlock.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                HintSurnameTextBlock.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void NameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (NameTextBox.Text.Length > 0)
+            {
+                HintNameTextBlock.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                HintNameTextBlock.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void MiddlenameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (MiddlenameTextBox.Text.Length > 0)
+            {
+                HintMiddlenameTextBlock.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                HintMiddlenameTextBlock.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void EmailTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (EmailTextBox.Text.Length > 0)
+            {
+                HintEmailTextBlock.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                HintEmailTextBlock.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void PhoneTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (PhoneTextBox.Text.Length > 0)
+            {
+                HintPhoneTextBlock.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                HintPhoneTextBlock.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void NewPasswordTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (NewPasswordTextBox.Text.Length > 0)
+            {
+                HintNewPasswordTextBlock.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                HintNewPasswordTextBlock.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void PasswordPaswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (PasswordPaswordBox.Password.Length > 0)
+            {
+                HintPasswordTextBlock.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                HintPasswordTextBlock.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void RegistrationButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AuthorizationButton_Click(object sender, RoutedEventArgs e)
+        {
+            AuthorizationWindow authorizationWindow = new AuthorizationWindow();
+            authorizationWindow.Show();
+            this.Close();
         }
 
         #region Управление окном
